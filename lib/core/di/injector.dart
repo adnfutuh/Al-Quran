2f8434927369af7d10385fb5c9f85194513bedd1
rizki <rizkiadnanfutuh@gmail.com>
2024-05-24 13:28:53 +0700

    feat: create and register http service

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
-import 'injector.config.dart';
+import '../injector.config.dart';
 
 @InjectableInit(
   initializerName: r'$initGetIt',
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alquran_app/core/injector.dart';
+import 'package:alquran_app/core/di/injector.dart';
 import 'package:alquran_app/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
