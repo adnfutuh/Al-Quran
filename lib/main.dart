b00c40c0de21a86e11ba917c0405d452d9d1462d
rizki <rizkiadnanfutuh@gmail.com>
2024-05-27 22:50:56 +0700

    feat: add screenutils, and design systems

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:alquran_app/core/di/injector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routes/routes_config.dart';
 
 void main() async {
+  await ScreenUtil.ensureScreenSize();
   await configureDependencies();
 
   runApp(const MyApp());
@@ -13,8 +15,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: RoutesConfig.appRouter,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp.router(
+        routerConfig: RoutesConfig.appRouter,
+      ),
     );
   }
 }
