01b4fbd9dcb57a8f158f62d7b549f01629f6eebb
rizki <rizkiadnanfutuh@gmail.com>
2024-05-27 22:28:40 +0700

    feat: create go router

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,34 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/home_header_widget.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Column(
-        children: [
-          Center(
-            child: Text('Banner'),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-          Center(
-            child: Text('Feature Widget'),
-          ),
-          SizedBox(height: 90),
-          Center(
-            child: Text(' Sholat Time Widget'),
-          ),
-          Spacer(),
-          Center(
-            child: Text('Bottom Navigation'),
-          ),
-        ],
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            HomeHeaderWidget(),
+            SizedBox(height: 100),
+            Center(
+              child: Text('Feature Widget'),
+            ),
+            SizedBox(height: 90),
+            Center(
+              child: Text(' Sholat Time Widget'),
+            ),
+            Spacer(),
+            Center(
+              child: Text('Bottom Navigation'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:alquran_app/core/di/injector.dart';
-import 'package:alquran_app/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'core/routes/routes_config.dart';
 
 void main() async {
   await configureDependencies();
@@ -10,16 +11,10 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+    return MaterialApp.router(
+      routerConfig: RoutesConfig.appRouter,
     );
   }
 }
