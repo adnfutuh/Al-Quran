8ca8bfa6beab198c98d34556e43cf0f873d13c56
rizki <rizkiadnanfutuh@gmail.com>
2024-05-30 13:33:36 +0700

    feat: implement cubit to ui

--- lib/features/prayer/presentation/ui/widgets/prayer_time_widget.dart
+++ lib/features/prayer/presentation/ui/widgets/prayer_time_widget.dart
@@ -1,19 +1,37 @@
+import 'package:alquran_app/features/prayer/presentation/cubit/prayer_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
 
-class PrayerTimeWidget extends StatelessWidget {
+@singleton
+class PrayerTimeWidget extends StatefulWidget {
   const PrayerTimeWidget({super.key});
+  @override
+  State<PrayerTimeWidget> createState() => _PrayerTimeWidgetState();
+}
+
+class _PrayerTimeWidgetState extends State<PrayerTimeWidget> {
+  @override
+  void initState() {
+    GetIt.I<PrayerCubit>().getLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text(
-          'Dzuhur 11.57',
-        ),
-        Text(
-          '10 menit lagi',
-        ),
-      ],
+    return BlocProvider.value(
+      value: GetIt.I<PrayerCubit>(),
+      child: const Column(
+        children: [
+          Text(
+            'Dzuhur 11.57',
+          ),
+          Text(
+            '10 menit lagi',
+          ),
+        ],
+      ),
     );
   }
 }
