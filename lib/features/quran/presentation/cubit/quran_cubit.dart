c5c7eb66081b5a884b84ad84eabce0c2f74fb116
rizki <rizkiadnanfutuh@gmail.com>
2024-05-30 19:55:43 +0700

    feat: create cubit for controlling getQuran Usecase and handling errors

--- lib/features/quran/presentation/cubit/quran_cubit.dart
+++ lib/features/quran/presentation/cubit/quran_cubit.dart
@@ -1,10 +1,27 @@
 import 'package:alquran_app/core/core.dart';
+import 'package:alquran_app/features/quran/data/models/surah_model.dart';
+import 'package:alquran_app/features/quran/domain/usecases/get_quran.dart';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'quran_state.dart';
 part 'quran_cubit.freezed.dart';
 
+@singleton
 class QuranCubit extends Cubit<QuranState> {
-  QuranCubit() : super(const QuranState.initial());
+  final GetQuran getQuran;
+  QuranCubit(this.getQuran) : super(const QuranState.initial());
+
+  void getQuranData(String id) async {
+    emit(const QuranState.loading());
+    final result = await getQuran(id);
+    result.fold(
+      (error) {
+        emit(QuranState.error(error: error));
+      },
+      (result) => emit(QuranState.success(surahModel: result)),
+    );
+  }
 }
